7a5285e64094c42a3df0eabc4a3088419b48e1c2
AntonioCelotto <a.celotto@newdigitalapp.com>
2026-09-17 12:38:24 +0200

    Connect avatar page to its cloud knowledge

--- app/avatar/[slug]/page.js
+++ app/avatar/[slug]/page.js
@@ -1,9 +1,12 @@
 import CustomAvatar from "./custom-avatar";
-import { getAvatarClientBySlug } from "../../lib/supabase-server";
+import { getAvatarClientBySlug, getAvatarKnowledgeText } from "../../lib/supabase-server";
 
 export default async function CustomAvatarPage({ params }) {
   const { slug } = await params;
   let cloudAvatar = null;
-  try { cloudAvatar = await getAvatarClientBySlug(slug); } catch {}
+  try {
+    cloudAvatar = await getAvatarClientBySlug(slug);
+    if (cloudAvatar?.id) cloudAvatar.website_knowledge = await getAvatarKnowledgeText(cloudAvatar.id);
+  } catch {}
   return <CustomAvatar slug={slug} initialAvatar={cloudAvatar} />;
 }
